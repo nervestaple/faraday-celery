811edba8f9cb11b39913d003e2292c0c94fa6ca9
Jim McGee <nervestaple@gmail.com>
2024-10-27 23:33:55 -0400

    fix playwright path

--- playwright_setup.sh
+++ playwright_setup.sh
@@ -6,7 +6,7 @@ playwright install chromium
 # Store/pull Playwright cache with build cache
 if [[ ! -d $PLAYWRIGHT_BROWSERS_PATH ]]; then 
   echo "...Copying Playwright Cache from Build Cache" 
-  cp -R $XDG_CACHE_HOME/playwright/ $PLAYWRIGHT_BROWSERS_PATH
+  cp -R $XDG_CACHE_HOME/ms-playwright/ $PLAYWRIGHT_BROWSERS_PATH
 else 
   echo "...Storing Playwright Cache in Build Cache" 
   cp -R $PLAYWRIGHT_BROWSERS_PATH $XDG_CACHE_HOME
